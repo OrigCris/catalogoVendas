69f7aecd421bfc8d4d4641a289568c376be2b375
Cristiano Alves <cristianosjunior2012@hotmail.com>
2024-08-10 20:42:22 -0300

    att job

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def form():
     return render_template('index.html')
+
+@app.route('/index', methods=['POST'])
+def confirm():
+    name = request.form['name']
+    return render_template('confirm.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
